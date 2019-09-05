87769b43c8040766f44cd64a29fe37f9e2d55d14
KoenVermeulen91 <vermeulen.koen@me.com>
2019-09-05 07:50:13 +0200

    1st commit

---
+++ Rough draft.R
@@ -0,0 +1,5 @@
+#### Course Project: Regression Models ####
+
+# Setting working directory
+setwd("/Users/Koen Vermeulen/Downloads/Data_Science_Specialization/Tests/Tests_R/Course_Project-Regression_models")
+
